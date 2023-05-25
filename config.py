cbf91cd4a3b5efdc33339ee9810bf5b67f80b677
igruk <88056536+igruk@users.noreply.github.com>
2023-05-25 03:16:45 +0300

    refactoring

--- config.py
+++ config.py
@@ -8,8 +8,7 @@ CONFIG = {
         'check_button_text': 'Include Shortcuts and Folders',
         'label_text': 'Clear Desktop in one Click!',
         'info_title': 'Info',
-        'file_exists_message': 'File with name "{file}" already exists and won\'t be moved.',
-        'folder_exists_message': 'Folder with name "{folder}" already exists and won\'t be moved.',
+        'file_exists_message': '"{file}" already exists and won\'t be moved.',
     },
     'ua': {
         'language_button_text': 'UA / EN',
@@ -18,7 +17,6 @@ CONFIG = {
         'check_button_text': 'Включно з ярликами і папками',
         'label_text': 'Очисти Робочий стіл в один клік!',
         'info_title': 'Інформація',
-        'file_exists_message': 'Файл "{file}" вже існує і не буде переміщений.',
-        'folder_exists_message': 'Папка "{folder}" вже існує і не буде переміщена.',
+        'file_exists_message': '"{file}" вже існує і не буде переміщено.',
     }
 }
